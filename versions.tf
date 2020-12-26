9434629026459ca5233de4447346a02475d1f6c5
Mike Brancato <mike@mikebrancato.com>
2020-12-25 23:36:04 -0500

    Add new provider version format

---
+++ versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+  required_version = ">= 0.13"
+}
